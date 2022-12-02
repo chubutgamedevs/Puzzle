52f1032f1a55f3ea6ac03ff45ed6bda9961e00ee
PCRGB <gdvestudios@gmail.com>
2022-12-02 11:28:44 -0300

    puzzle

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -14,6 +14,7 @@ public class GameManager : MonoBehaviour
     private int slots;
     private float escala;
     private Vector3  posSlot = Vector3.one;
+    [SerializeField] private GameObject Tablero;
     
     
     private RectTransform rt;
@@ -23,10 +24,11 @@ public class GameManager : MonoBehaviour
         rt = ItemSlot.GetComponent<RectTransform>();
         
         slots = grillaX + grillaY;
-        escala = Screen.width/grillaY;
+        escala =  (Tablero.GetComponent<RectTransform>().sizeDelta.y * -0.1f) / grillaY;
+        Debug.Log(Tablero.GetComponent<RectTransform>().sizeDelta.y);
 
-        Ancho = rt.rect.width * rt.localScale.x;
-        Largo = rt.rect.height * rt.localScale.y;
+        Ancho = rt.rect.width;
+        Largo = rt.rect.height;
         Debug.Log("Ancho: " + Ancho + " Largo: " + Largo + " Escala: " + escala);
 
         GenerarGrilla();
@@ -34,17 +36,20 @@ public class GameManager : MonoBehaviour
 
     private void GenerarGrilla(){
         
+        Transform Tablero = GameObject.FindGameObjectWithTag("Canvas").transform;
+        ItemSlot.transform.localScale =  Vector2.one;//new Vector2(escala,escala);
+
         while (cX != grillaX){
             while (cY != grillaY){
-                posSlot = new Vector3(this.transform.position.x + Ancho*cX,this.transform.position.y + Largo*cY,0)*-1;
-                Debug.Log(posSlot);
-                Instantiate(ItemSlot, posSlot , Quaternion.identity, GameObject.FindGameObjectWithTag("Canvas").transform);
+                posSlot = new Vector3(Tablero.position.x + Ancho*cX, Tablero.position.y + Largo*cY,0)*-1;
+                posSlot = Tablero.localToWorldMatrix * posSlot;
+                Instantiate(ItemSlot, posSlot , Quaternion.identity, Tablero);
                 cY = cY+1;
             }
             cX = cX+1;
             cY = 0;
         }
-         for(int i = 0; i < slots; i++)
+        for(int i = 0; i < slots; i++)
         {
         // Code to be repeated.
         }
--- Assets/Slot.cs
+++ Assets/Slot.cs
@@ -5,11 +5,16 @@ using UnityEngine.EventSystems;
 
 public class Slot : MonoBehaviour, IDropHandler
 {
+    Transform canvaT;
+    private void Awake() {
+        canvaT = GameObject.FindGameObjectWithTag("Canvas").transform;
+    }
     public void OnDrop(PointerEventData eventData)
     {
         Debug.Log("OnDrop"); 
         if (eventData.pointerDrag != null){
             eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+            Debug.Log("Soltado en un slot "+ GetComponent<RectTransform>().anchoredPosition);
         }
     }
 }
